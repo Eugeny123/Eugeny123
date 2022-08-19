b3d13177f059e5b480522d9675f1d3d26deb3041
Eugeny <sgenya72@gmail.com>
2022-08-19 22:59:17 +0300

    complited four tasks

---
+++ HomeWorkSeminar01C#/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+//task number two C# seminar
+//enter variabls
+int A = -9;
+int B = -3;
+int max;
+
+if (A > B) //comparing variables
+{
+    max = A;
+}
+else
+{
+    max = B;
+}
+Console.WriteLine(max); //output
+
+*/
+
+/*
+//task number four C# seminar
+//enter variabls
+int A = 2;
+int B = 3;
+int C = 7;
+int max;
+
+if (A > B){  //comparing variables
+    max = A;
+}else{
+    max = B;
+}
+if (C > max){ //define "max"
+    max = C;
+}
+Console.WriteLine(max); //output
+
+*/
+
+/*
+//task number six C# seminar
+//enter variabl
+int a;
+// enter number
+a = Convert.ToInt32(Console.ReadLine());
+//even or odd function C#
+if (a % 2 == 0){
+    Console.WriteLine("Aw this number is EVEN ");
+}else{
+    Console.WriteLine("Oh this is ODD ");
+}
+
+*/
+
+
+/*
+//task number six C# seminar
+//enter variabls
+int N;
+int count = 1;
+// enter number N
+    Console.WriteLine("Please enter number: ");
+    N = Convert.ToInt32(Console.ReadLine());
+//add cycle operator    
+while (count <= N){
+if (count % 2 == 0){
+Console.Write(" "+ count); //output result
+}   
+count++; //created counter
+}
+*/
